f26176f43b59f22cf0708adfa249aaef1d3f6c44
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-16 09:12:57 +0300

    Header Icons add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from './../assets/Images/logo.png'
+import { HiHome,
+    HiMagnifyingGlass,
+    HiStar,
+    HiPlayCircle,
+    HiTv } from "react-icons/hi2";
+import { HiPlus,HiDotsVertical } from "react-icons/hi";
+import HeaderItems from './HeaderItems';
 
 const Header = () => {
+    const [toggle,setToggle]=useState(false);
+    const menu=[
+        {
+            name:'HOME',
+            icon:HiHome
+        },
+        {
+            name:'SEARCH',
+            icon:HiMagnifyingGlass
+        },
+        {
+            name:'WATCH LIST',
+            icon:HiPlus
+        },
+        {
+            name:'ORIGINALS',
+            icon:HiStar
+        },
+        {
+            name:'MOVIES',
+            icon:HiPlayCircle
+        },
+        {
+            name:'SERIES',
+            icon:HiTv
+        }
+    ]    
   return (
     <div>
-      header 
+      <img src={logo} className='w-[80px] md:w-[115px] object-cover' />
+      {menu.map((item) =>(
+        <HeaderItems name={item.name} Icon={item.icon}/>
+      ))}
     </div>
   )
 }
