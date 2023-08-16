76abecd5173152b9d78b88c76e370a6237b01d30
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-16 09:49:36 +0300

    Responsive Header and Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,9 +40,25 @@ const Header = () => {
     <div className='flex items-center justify-between p-5'>
       <div className='flex items-center gap-8'> 
       <img src={logo} className='w-[80px] md:w-[115px] object-cover' />
+      <div className='hidden md:flex gap-8'>
       {menu.map((item) =>(
         <HeaderItems name={item.name} Icon={item.icon}/>
       ))}
+      </div>
+      <div className='flex md:hidden gap-5'>
+      {menu.map((item, index) =>index<3&&(
+        <HeaderItems name={''} Icon={item.icon}/>
+      ))}
+      <div className='md:hidden' onClick={()=>setToggle(!toggle)}>       
+            <HeaderItems name={''} Icon={HiDotsVertical} />
+           {toggle? <div className='absolute mt-3 bg-[#121212] 
+            border-[1px] border-gray-700 p-3 px-5 py-4'>
+            {menu.map((item,index)=>index>2&&(
+            <HeaderItems name={item.name} Icon={item.icon} />
+            ))}
+            </div>:null}
+            </div> 
+      </div>
       </div> 
       <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
         className='w-[40px] rounded-full'/>
