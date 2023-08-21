431c848b5ea28c94761fcf47cfdd47aca4dcdf5c
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-21 04:48:21 +0300

    Responsive Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../Api/GlobalApi'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+const screenWidth = window.innerWidth;
 
 
 const Slider = () => {
-    const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
     const [movieList, setMovieList]=useState([]);
+    const elementRef = useRef();
     useEffect(() => {
         getTrendingMovies();
     },[])
@@ -14,11 +17,29 @@ const Slider = () => {
             setMovieList(resp.data.results)
         })
     } 
+
+    const sliderRight = (element) => {
+        element.scrollLeft+=screenWidth-110
+    }
+    const sliderLeft = (element) => {
+        element.scrollLeft-=screenWidth-100
+    }
   return (
     <div>
-      {movieList.map((item)=>(
-        <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full h-[310px]'/>
-      ))}
+     <HiChevronLeft className="hidden md:block text-white text-[30px] absolute
+        mx-8 mt-[150px] cursor-pointer " 
+        onClick={()=>sliderLeft(elementRef.current)}/>
+        <HiChevronRight className='hidden md:block text-white text-[30px] absolute
+        mx-8 mt-[150px] cursor-pointer right-0' 
+        onClick={()=>sliderRight(elementRef.current)}/>
+    <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth' ref={elementRef}>
+        {movieList.map((item) => (
+            <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full md:h-[310px] object-cover 
+            object-left-top mr-5 rounded-md hover:border-[4px] 
+            border-gray-400 transition-all duration-100 ease-in'/>
+        ))}
+
+    </div>
     </div>
   )
 }
