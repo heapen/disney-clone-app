f9457348cd9dbd460049e7a522112961a077a2a6
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-21 06:25:45 +0300

    MovieCard finish

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -5,10 +5,10 @@ import MovieList from './MovieList'
 const GenreMovieList = () => {
   return (
     <div>
-      {GenresList.genere.map((item,index) =>index<4&&(
+      {GenresList.genere.map((item,index) =>index<5&&(
         <div className='p-8 px-8 md:px-16'>
             <h2 className='text-[20px] text-white font-bold'>{item.name}</h2>
-            <MovieList genreId={item.id}/>
+            <MovieList genreId={item.id} index_={index}/>
         </div>
       ))}
     </div>
