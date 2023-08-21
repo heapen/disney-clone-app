e693f5e2dccc6118a13c6e689068d764ee343dec
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-21 05:38:47 +0300

    GenreMovieList create

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Header from './components/Header'
 import Slider from './components/Slider'
 import ProductionHouse from './components/ProductionHouse'
+import GenreMovieList from './components/GenreMovieList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,8 @@ function App() {
           <Slider/>
 
           <ProductionHouse/>
+
+          <GenreMovieList/>
       </div>
     </>
   )
