a7dc35e3ee85b6a57d3d75f615f5d3b08705b2d4
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-21 04:01:29 +0300

    API and Slider test, Tailwind Css Scrollhide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import GlobalApi from '../Api/GlobalApi'
 
+
 const Slider = () => {
+    const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+    const [movieList, setMovieList]=useState([]);
     useEffect(() => {
         getTrendingMovies();
     },[])
     const getTrendingMovies=()=>{
         GlobalApi.getTrendingVideos.then(resp=>{
-            console.log(resp)
+            console.log(resp.data.results)
+            setMovieList(resp.data.results)
         })
     } 
   return (
     <div>
-      
+      {movieList.map((item)=>(
+        <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full h-[310px]'/>
+      ))}
     </div>
   )
 }
